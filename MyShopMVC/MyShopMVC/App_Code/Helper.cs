6ffb7febe10c84dd8b52df4c3192a08ecc2be86b
Abram John Limpin <abramjohn.limpin@benilde.edu.ph>
2017-12-14 11:04:45 +0800

    Added Helper class; Created method to return connection string value

---
+++ MyShopMVC/MyShopMVC/App_Code/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace MyShopMVC.App_Code
+{
+    public class Helper
+    {
+        /// <summary>
+        /// Returns the connection string value from web.config file
+        /// </summary>
+        /// <returns>Database connection string</returns>
+        public static string GetConnection()
+        {
+            return ConfigurationManager.ConnectionStrings["MyCon"].ConnectionString;
+        }
+    }
+}
