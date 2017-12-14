4a50b8412d49a031effeb4940aac449c98feedea
Abram John Limpin <abramjohn.limpin@benilde.edu.ph>
2017-12-14 12:31:50 +0800

    Applied Font Awesome icons to list page

--- MyShopMVC/MyShopMVC/App_Start/BundleConfig.cs
+++ MyShopMVC/MyShopMVC/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace MyShopMVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/font-awesome.min.css"));
         }
     }
 }
