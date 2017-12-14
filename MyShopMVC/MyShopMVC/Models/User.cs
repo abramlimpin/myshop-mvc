c245f134f1be8764bcaded62668e6eaa83d2a6d0
Abram John Limpin <abramjohn.limpin@benilde.edu.ph>
2017-12-14 11:10:18 +0800

    Added list of property names (Users, Types)

---
+++ MyShopMVC/MyShopMVC/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyShopMVC.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public int TypeID { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FN { get; set; }
+        public string LN { get; set; }
+        public string Street { get; set; }
+        public string Municipality { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+        public string Status { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime? DateModified { get; set; }
+    }
+}
