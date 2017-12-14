78bc09d33a2ddc4becc86fb047c4674cd4a40189
Abram John Limpin <abramjohn.limpin@benilde.edu.ph>
2017-12-14 11:28:55 +0800

    Added controller (Users); Added method to display list of user types

---
+++ MyShopMVC/MyShopMVC/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using MyShopMVC.App_Code;
+using MyShopMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShopMVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        /// <summary>
+        /// Displays list of user types from Types table
+        /// </summary>
+        /// <returns></returns>
+        public List<UserType> GetUserTypes()
+        {
+            var list = new List<UserType>();
+            using (SqlConnection con = new SqlConnection(Helper.GetConnection()))
+            {
+                con.Open();
+                string query = @"SELECT TypeID, UserType FROM UserTypes
+                    ORDER BY UserType";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    using (SqlDataReader data = cmd.ExecuteReader())
+                    {
+                        while (data.Read())
+                        {
+                            list.Add(new UserType
+                            {
+                                ID = int.Parse(data["TypeID"].ToString()),
+                                Name = data["UserType"].ToString()
+                            });
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
